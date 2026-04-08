6c344c9b93e8319239d509225f3c80febc5f4c53
lilleprinsen-dotcom <lilleprinsen@gmail.com>
2026-04-08 10:17:29 +0200

    Extract estimation logic into estimator service

--- lilleprinsen-cargonizer-connector.php
+++ lilleprinsen-cargonizer-connector.php
@@ -13,6 +13,7 @@ if (!defined('ABSPATH')) {
 require_once __DIR__ . '/includes/class-lp-cargonizer-connector.php';
 require_once __DIR__ . '/includes/class-lp-cargonizer-api-service.php';
 require_once __DIR__ . '/includes/class-lp-cargonizer-settings-service.php';
+require_once __DIR__ . '/includes/class-lp-cargonizer-estimator-service.php';
 require_once __DIR__ . '/includes/class-lp-cargonizer-plugin.php';
 
 $lp_cargonizer_plugin = new LP_Cargonizer_Plugin();
